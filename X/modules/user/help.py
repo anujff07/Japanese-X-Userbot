e980258e14b153a17d685226b07440a807d46d8b
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <156512147+nobitaaxd@users.noreply.github.com>
2024-03-10 22:51:39 +0530

    Update help.py

--- X/modules/user/help.py
+++ X/modules/user/help.py
@@ -44,7 +44,7 @@ async def module_help(client: Client, message: Message):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @Japanese_Support_Chat × @Nobitaa_xd•",
+                f"```{str(ac)}```\n• @Japanese_Userbot_Chat × @Nobitaa_xd•",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
