a4db88d3fa800889de746c976e36244cf589152d
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <156512147+nobitaaxd@users.noreply.github.com>
2024-03-13 13:01:58 +0530

    Update pmpermit.py

--- X/modules/user/pmpermit.py
+++ X/modules/user/pmpermit.py
@@ -1,13 +1,3 @@
-# Credits: @mrismanaziz
-# Copyright (C) 2022 Pyro-ManUserbot
-#
-# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
-#
-# t.me/SharingUserbot & t.me/Lunatic0de
-
-
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 from sqlalchemy.exc import IntegrityError
