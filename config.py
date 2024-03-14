253ffe5dc4e82eb68b4c2fa28f060ca7991cb176
MirzaElite <126695903+MirzaElite@users.noreply.github.com>
2024-03-14 14:06:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ if not BLACKLIST_CHAT:
 BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 BOT_VER = "1.1.5@main"
-BRANCH = getenv("BRANCH", "main") #don't change
+BRANCH = getenv("BRANCH", "main") #don't change this line 
 CMD_HNDLR = cmd
 OWNER_ID = getenv("OWNER_ID", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "none")
