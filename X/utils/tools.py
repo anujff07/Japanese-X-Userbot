165161cda69103219bb1e85f58749bc7221d55f8
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <156512147+nobitaaxd@users.noreply.github.com>
2024-03-16 10:09:29 +0530

    Update tools.py

--- X/utils/tools.py
+++ X/utils/tools.py
@@ -22,7 +22,7 @@ async def get_readable_time(seconds: int) -> str:
     count = 0
     up_time = ""
     time_list = []
-    time_suffix_list = ["s", "m", "Jam", "Hari"]
+    time_suffix_list = ["s", "m", "Jam", "Day"]
 
     while count < 4:
         count += 1
